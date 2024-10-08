788dbcf591840d4d5f363826d4a55f6d1e6af407
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-10-08 15:57:23 +0200

    Progress on About page layout and styles

---
+++ app/javascript/scrollmagic.js
@@ -0,0 +1,4 @@
+import ScrollMagic from "scrollmagic";
+import "scrollmagic/plugins/animation.gsap";
+import "scrollmagic/plugins/debug.addIndicators";
+import { gsap } from "gsap";
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,9 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+
+# ScrollMagic
+pin "scrollmagic", to: "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"
+pin "scrollmagic/plugins/animation.gsap", to: "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.min.js"
+pin "scrollmagic/plugins/debug.addIndicators", to: "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"
+pin "gsap", to: "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.6/gsap.min.js"
