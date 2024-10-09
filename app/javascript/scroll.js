9cbeb05c99ca40d2bf09d6cba11ea81a1cc9b12e
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-10-09 19:14:10 +0200

    scrollMagic css reglage turbo

--- app/javascript/scroll.js
+++ app/javascript/scroll.js
@@ -1,22 +1,24 @@
-const allRonds = document.querySelectorAll(".rond");
-const allBoxes = document.querySelectorAll(".box");
+document.addEventListener("turbo:load", function () {
+  const allRonds = document.querySelectorAll(".rond");
+  const allBoxes = document.querySelectorAll(".box");
 
-const controller = new ScrollMagic.Controller();
+  const controller = new ScrollMagic.Controller();
 
-allBoxes.forEach((box) => {
-  for (let i = 0; i < allRonds.length; i++) {
-    if (allRonds[i].getAttribute("data-anim") === box.getAttribute("data-anim")) {
-      let tween = gsap.from(box, { y: -50, opacity: 0, duration: 0.5 });
+  allBoxes.forEach((box) => {
+    for (let i = 0; i < allRonds.length; i++) {
+      if (allRonds[i].getAttribute("data-anim") === box.getAttribute("data-anim")) {
+        let tween = gsap.from(box, { y: -50, opacity: 0, duration: 0.5 });
 
-      let scene = new ScrollMagic.Scene({
-        triggerElement: allRonds[i],
-        reverse: false,
-      })
-        .setTween(tween)
-        .addIndicators()
-        .addTo(controller);
+        let scene = new ScrollMagic.Scene({
+          triggerElement: allRonds[i],
+          reverse: false,
+        })
+          .setTween(tween)
+          .addIndicators() // Vous pouvez enlever cette ligne si vous ne voulez pas les indicateurs de débogage
+          .addTo(controller);
+      }
     }
-  }
-});
+  });
 
-console.log("Hello from scroll.js");
+  console.log("Hello from scroll.js");
+});
