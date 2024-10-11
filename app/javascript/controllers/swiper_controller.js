5b35882b17849d33cac5e735f8b5bad602054fae
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-10-11 15:11:05 +0200

    swiper images on show project

---
+++ app/javascript/controllers/swiper_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="swiper"
+export default class extends Controller {
+  connect() {
+    console.log("Swiper initialized");
+
+    // Utiliser Swiper directement
+    const swiper = new Swiper(".mySwiper", {
+      loop: true,
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+    const swiper2 = new Swiper(".mySwiper2", {
+      loop: true,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      thumbs: {
+        swiper: swiper,
+      },
+    });
+  }
+}
