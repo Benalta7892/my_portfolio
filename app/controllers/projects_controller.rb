aeac38f34be66091357f83909715d7398a3c545d
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-10-17 16:24:41 +0200

    front end css

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,15 +1,19 @@
 class ProjectsController < ApplicationController
   def index
+    @admin_user = User.find_by(admin: true)
     @projects = Project.order(created_at: :asc)
     @title = "Portfolio"
     @subtitle = "Découvrez tous mes projets"
+    @resume = Resume.find_by(user: @admin_user)
   end
 
   def show
+    @admin_user = User.find_by(admin: true)
     @project = Project.find(params[:id])
 
     @title = @project.title
     @subtitle = @project.subtitle
+    @resume = Resume.find_by(user: @admin_user)
   end
 
   def new
