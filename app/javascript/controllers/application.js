e3bfece3f6cf29beb937e9dddfe9ad19f916d3a5
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-10-24 16:28:37 +0200

    Vérification

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,9 @@
 import { Application } from "@hotwired/stimulus";
-import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
 
-// Crée une instance de l'application Stimulus
 const application = Application.start();
 
-// Charge tous les contrôleurs présents dans le dossier controllers
-const context = require.context("./controllers", true, /\.js$/);
-application.load(definitionsFromContext(context));
+// Configure Stimulus development experience
+application.debug = false;
+window.Stimulus = application;
+
+export { application };
