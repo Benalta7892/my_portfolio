1fe82c31d42fb585dc75a562ada2d68213ecbb6a
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-10-24 13:13:18 +0200

    production heroku config

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'ffi', '< 1.17.0'
 
 gem "cloudinary"
 
-gem 'letter_opener'
+
 
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
@@ -69,6 +69,9 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
+
+  # Letter Opener for previews in development
+  gem 'letter_opener'
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "https://my-portfolio-benoit-0489a59121b6.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -73,6 +73,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "my_portfolio_production"
 
   config.action_mailer.perform_caching = false
+
+    # SMTP configuration for Gmail
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      user_name: ENV["GMAIL_USERNAME"],
+      password: ENV["GMAIL_PASSWORD"],
+      authentication: "plain",
+      enable_starttls_auto: true
+    }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
