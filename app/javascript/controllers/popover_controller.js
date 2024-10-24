aa90ac713ded51c141d5bb52b13858ad9075dce8
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-10-24 04:34:47 +0200

    stimulus popover on logo

--- app/javascript/controllers/popover_controller.js
+++ app/javascript/controllers/popover_controller.js
@@ -1,27 +1,33 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["content", "card"]; // Ajout de "content" en cible
+  static targets = ["content", "card"];
 
   connect() {
     console.log("Popover initialized");
+    this.showTimeout = null; // Pour stocker le timeout de show
+    this.hideTimeout = null; // Pour stocker le timeout de hide
   }
 
   show() {
     console.log("Attempting to show popover");
 
-    // Vérifier si le contenu du template est disponible
+    if (this.hideTimeout) {
+      clearTimeout(this.hideTimeout); // Annuler la tentative de cacher si elle est en cours
+    }
+
     if (this.hasContentTarget) {
-      // Cloner le contenu du template s'il n'a pas été déjà inséré
       if (!this.hasCardTarget) {
         const card = this.contentTarget.content.cloneNode(true);
         this.element.appendChild(card);
       }
 
-      // Afficher la carte si elle existe maintenant
       if (this.hasCardTarget) {
-        this.cardTarget.style.display = "block";
-        console.log("Popover displayed:", this.cardTarget);
+        // Ajoutez un léger délai pour éviter le spam de l'événement
+        this.showTimeout = setTimeout(() => {
+          this.cardTarget.classList.add("popover-visible"); // Ajoute la classe pour l'afficher avec la transition
+          console.log("Popover displayed:", this.cardTarget);
+        }, 200); // Un petit délai pour initier l'affichage
       } else {
         console.error("cardTarget is not defined or found after cloning");
       }
@@ -32,8 +38,16 @@ export default class extends Controller {
 
   hide() {
     console.log("Hiding popover");
-    if (this.hasCardTarget) {
-      this.cardTarget.style.display = "none";
+
+    if (this.showTimeout) {
+      clearTimeout(this.showTimeout); // Annuler la tentative d'afficher si elle est en cours
     }
+
+    this.hideTimeout = setTimeout(() => {
+      if (this.hasCardTarget) {
+        this.cardTarget.classList.remove("popover-visible"); // Retire la classe pour cacher avec transition
+        console.log("Popover hidden:", this.cardTarget);
+      }
+    }, 100); // Un délai léger avant de cacher l'élément
   }
 }
