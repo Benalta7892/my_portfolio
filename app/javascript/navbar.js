66b066c6f2a213f0ba0b6a3e60cd754baff893a7
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-10-26 12:06:18 +0200

    css front end navbar responsive ok

--- app/javascript/navbar.js
+++ app/javascript/navbar.js
@@ -1,21 +1,23 @@
-const hamburgerToggler = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
+document.addEventListener("turbo:load", () => {
+  const hamburgerToggler = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
 
-const toggleNav = () => {
-  hamburgerToggler.classList.toggle("open");
+  const toggleNav = () => {
+    hamburgerToggler.classList.toggle("open");
 
-  const ariaToggle = hamburgerToggler.getAttribute("aria-expanded") === "true" ? "false" : "true";
-  hamburgerToggler.setAttribute("aria-expanded", ariaToggle);
+    const ariaToggle = hamburgerToggler.getAttribute("aria-expanded") === "true" ? "false" : "true";
+    hamburgerToggler.setAttribute("aria-expanded", ariaToggle);
 
-  navLinks.classList.toggle("open");
-};
+    navLinks.classList.toggle("open");
+  };
 
-hamburgerToggler.addEventListener("click", toggleNav);
+  hamburgerToggler.addEventListener("click", toggleNav);
 
-new ResizeObserver((entries) => {
-  if (entries[0].contentRect.width <= 650) {
-    navLinks.style.transition = "transform 0.3s ease-out";
-  } else {
-    navLinks.style.transition = "none";
-  }
-}).observe(document.body);
+  new ResizeObserver((entries) => {
+    if (entries[0].contentRect.width <= 650) {
+      navLinks.style.transition = "transform 0.3s ease-out";
+    } else {
+      navLinks.style.transition = "none";
+    }
+  }).observe(document.body);
+});
