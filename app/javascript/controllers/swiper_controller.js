2770baed6575f803c79797bea8d0478ecc40cb07
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-10-31 16:35:41 +0100

    Fix css size on carousel

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -42,52 +42,52 @@ export default class extends Controller {
       },
     });
 
-    // Ajuster la hauteur initialement et lors des changements de slide
-    this.swiper2.on("slideChangeTransitionEnd", () => {
-      this.adjustContainerHeight(this.swiper2);
-      this.applyObjectFit();
-    });
+    // // Ajuster la hauteur initialement et lors des changements de slide
+    // this.swiper2.on("slideChangeTransitionEnd", () => {
+    //   this.adjustContainerHeight(this.swiper2);
+    //   this.applyObjectFit();
+    // });
 
-    // Ajuster la hauteur initialement après la première initialisation
-    this.adjustContainerHeight(this.swiper2);
-    this.applyObjectFit();
+    // // Ajuster la hauteur initialement après la première initialisation
+    // this.adjustContainerHeight(this.swiper2);
+    // this.applyObjectFit();
   }
 
-  // Fonction pour ajuster la hauteur du conteneur
-  adjustContainerHeight(swiperInstance) {
-    const container = document.querySelector(".mySwiper2");
-    const activeSlide = swiperInstance.slides[swiperInstance.activeIndex]?.querySelector("img");
+  // // Fonction pour ajuster la hauteur du conteneur
+  // adjustContainerHeight(swiperInstance) {
+  //   const container = document.querySelector(".mySwiper2");
+  //   const activeSlide = swiperInstance.slides[swiperInstance.activeIndex]?.querySelector("img");
 
-    if (container && activeSlide) {
-      // Réinitialiser la hauteur avant de définir la nouvelle
-      container.style.height = "";
+  //   if (container && activeSlide) {
+  //     // Réinitialiser la hauteur avant de définir la nouvelle
+  //     container.style.height = "";
 
-      // Mettre à jour la hauteur après le chargement de l'image active
-      const updateHeight = () => {
-        container.style.height = `${activeSlide.offsetHeight}px`;
-      };
+  //     // Mettre à jour la hauteur après le chargement de l'image active
+  //     const updateHeight = () => {
+  //       container.style.height = `${activeSlide.offsetHeight}px`;
+  //     };
 
-      // Écouter l'événement de chargement de l'image active
-      if (!activeSlide.complete) {
-        activeSlide.addEventListener("load", updateHeight);
-      } else {
-        // Si l'image est déjà chargée
-        updateHeight();
-      }
-    }
-  }
+  //     // Écouter l'événement de chargement de l'image active
+  //     if (!activeSlide.complete) {
+  //       activeSlide.addEventListener("load", updateHeight);
+  //     } else {
+  //       // Si l'image est déjà chargée
+  //       updateHeight();
+  //     }
+  //   }
+  // }
 
-  // Fonction pour appliquer l'objet fit en fonction de la hauteur de l'image et de la taille de l'écran
-  applyObjectFit() {
-    const images = document.querySelectorAll(".mySwiper2 img");
-    images.forEach((img) => {
-      if (window.innerWidth > 365 && img.offsetHeight > 290) {
-        // Utiliser 'contain' si la largeur de l'écran est supérieure à 500 et que l'image est grande
-        img.style.objectFit = "contain";
-      } else {
-        // Sinon, appliquer 'fill'
-        img.style.objectFit = "fill";
-      }
-    });
-  }
+  // // Fonction pour appliquer l'objet fit en fonction de la hauteur de l'image et de la taille de l'écran
+  // applyObjectFit() {
+  //   const images = document.querySelectorAll(".mySwiper2 img");
+  //   images.forEach((img) => {
+  //     if (window.innerWidth > 365 && img.offsetHeight > 290) {
+  //       // Utiliser 'contain' si la largeur de l'écran est supérieure à 500 et que l'image est grande
+  //       img.style.objectFit = "contain";
+  //     } else {
+  //       // Sinon, appliquer 'fill'
+  //       img.style.objectFit = "fill";
+  //     }
+  //   });
+  // }
 }
