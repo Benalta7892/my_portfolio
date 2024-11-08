a73110a9170669200c279da5879b31a6175afbf1
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-08 18:02:36 +0100

    Fix column existence check in migration

--- db/migrate/20241108162740_remove_cloudinary_url_from_technology_items.rb
+++ db/migrate/20241108162740_remove_cloudinary_url_from_technology_items.rb
@@ -1,5 +1,5 @@
 class RemoveCloudinaryUrlFromTechnologyItems < ActiveRecord::Migration[7.1]
   def change
-    remove_column :technology_items, :cloudinary_url, :string
+    remove_column :technology_items, :cloudinary_url, :string, if_exists: true
   end
 end
