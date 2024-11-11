0b613948ec4e5864b8d11f9d39157f191279d189
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-11 14:38:50 +0100

    vantajs for small hero banner

--- app/javascript/vanta.js
+++ app/javascript/vanta.js
@@ -1,27 +1,93 @@
-import "https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js";
+// Importation des fichiers nécessaires pour les effets Vanta
+import "https://cdnjs.cloudflare.com/ajax/libs/three.js/r124/three.min.js";
 import "https://cdnjs.cloudflare.com/ajax/libs/vanta/0.5.21/vanta.clouds.min.js";
+import "https://cdnjs.cloudflare.com/ajax/libs/vanta/0.5.21/vanta.birds.min.js";
+import "https://cdnjs.cloudflare.com/ajax/libs/vanta/0.5.21/vanta.waves.min.js";
+import "https://cdnjs.cloudflare.com/ajax/libs/vanta/0.5.21/vanta.net.min.js";
 
-let vantaEffectClouds;
+let vantaEffect;
 
 document.addEventListener("turbo:load", function () {
-  // Détruire l'instance Vanta existante s'il y en a une
-  if (vantaEffectClouds) vantaEffectClouds.destroy();
+  const vantaElement = document.getElementById("vantajs");
 
-  // Initialiser l'animation des nuages
-  vantaEffectClouds = VANTA.CLOUDS({
-    el: "#vantajs", // Sélecteur de l'élément cible pour les nuages
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.0,
-    minWidth: 200.0,
-    backgroundColor: 0xffffff, // Couleur de fond
-    skyColor: 0x68b8d7, // Couleur du ciel
-    cloudColor: 0xadc1de, // Couleur des nuages
-    cloudShadowColor: 0x183550, // Couleur des ombres des nuages
-    sunColor: 0xff9919, // Couleur du soleil
-    sunGlareColor: 0xff6633, // Couleur de l'éclat du soleil
-    sunlightColor: 0xff9933, // Couleur de la lumière solaire
-    speed: 0.42, // Vitesse de l'animation
-  });
+  // Si l'élément n'existe pas, on ne lance pas Vanta
+  if (!vantaElement) {
+    if (vantaEffect) {
+      vantaEffect.destroy();
+      vantaEffect = null;
+    }
+    return;
+  }
+
+  // Détruire l'instance Vanta existante avant d'en créer une nouvelle
+  if (vantaEffect) vantaEffect.destroy();
+
+  // Appliquer l'effet Vanta en fonction de la classe de la page
+  if (vantaElement.classList.contains("hero")) {
+    vantaEffect = VANTA.CLOUDS({
+      el: "#vantajs",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      backgroundColor: 0xffffff,
+      skyColor: 0x68b8d7,
+      cloudColor: 0xadc1de,
+      cloudShadowColor: 0x183550,
+      sunColor: 0xff9919,
+      sunGlareColor: 0xff6633,
+      sunlightColor: 0xff9933,
+      speed: 0.42,
+    });
+  } else if (vantaElement.classList.contains("pages-page")) {
+    vantaEffect = VANTA.BIRDS({
+      el: "#vantajs",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      backgroundColor: 0xebf8ff,
+      color1: 0x4dbfe3,
+      color2: 0xff3838,
+      colorMode: "lerp",
+      birdSize: 0.5,
+      wingSpan: 20,
+      separation: 10,
+      alignment: 20,
+      cohesion: 20,
+      quantity: 6.9,
+    });
+  } else if (vantaElement.classList.contains("projects-page")) {
+    vantaEffect = VANTA.WAVES({
+      el: "#vantajs",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
+      color: 0x5d7591,
+      shininess: 30,
+      waveHeight: 15,
+      waveSpeed: 0.5,
+      zoom: 0.65,
+    });
+  } else if (vantaElement.classList.contains("contacts-page")) {
+    vantaEffect = VANTA.NET({
+      el: "#vantajs",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      color: 0x5588,
+      backgroundColor: 0xebf8ff,
+      points: 20,
+      maxDistance: 70,
+      spacing: 90,
+    });
+  }
 });
